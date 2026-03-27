2d7abb759612773bcd2ad84a09955ea8a8b03482
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-03-28 00:15:21 +0800

    Replace LinkedList Demo with HashMap

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
-import { LinkedList } from "./LinkedList.js";
+import { HashMap } from "./HashMap.js"
 
-const list = new LinkedList();
+const test = new HashMap() // or HashMap() if using a factory
 
-list.append("dog");
-list.append("cat");
-list.append("parrot");
-list.append("hamster");
-list.append("snake");
-list.append("turtle");
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
 
-console.log(list.size());
-console.log(list.toString());
+console.log(test.length())
+console.log(test.tableSize);
 
+test.set('moon', 'silver')
 
-list.removeAt(1);
-console.log(list.size());
-console.log(list.toString())
-
-
-
+console.log(test.length())
+console.log(test.tableSize);
 
